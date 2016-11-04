9c80e09f79fafe2c402bf14e580628d7f96476f4
Prakhar <srivprakhar@gmail.com>
2016-11-04 14:41:41 +0100

    some tests

--- index.php
+++ index.php
@@ -1,2 +1,44 @@
 <?php
 
+require 'vendor/autoload.php';
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+
+$key = '43b315128e32ee8662b980740cb99990a12b431c';
+
+$httpClient = new GuzzleAdapter(new Client());
+$sparky = new SparkPost($httpClient, ['key'=>$key]);
+
+$promise = $sparky->transmissions->post([
+    'content' => [
+        //'from' => [
+        //    'name' => 'SparkPost Team',
+        //    'email' => 'from@sparkpostbox.com',
+        //],
+        //'subject' => 'First Mailing From PHP',
+        //'html' => '<html><body><h1>Congratulations, {{name}}!</h1><p>You just sent your very first mailing!</p></body></html>',
+        //'text' => 'Congratulations, {{name}}!! You just sent your very first mailing!',
+        'template_id' => 'test-template'
+    ],
+    'substitution_data' => ['name' => 'Prakhar'],
+    'recipients' => [
+        [
+            'address' => [
+                'name' => 'Kyrre',
+                'email' => 'ps@kortogkontant.no',
+            ],
+        ],
+    ],
+]);
+
+try {
+    $response = $promise->wait();
+    echo $response->getStatusCode()."\n";
+    print_r($response->getBody())."\n";
+} catch (\Exception $e) {
+    echo $e->getCode()."\n";
+    echo $e->getMessage()."\n";
+}
+
+echo "I will print out after the promise is fulfilled";
