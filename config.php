4bc97e252e90edfb29eb51b0b5fd19163bfd7dd9
Prakhar <srivprakhar@gmail.com>
2016-11-05 13:59:53 +0100

    Added examples for lists

---
+++ config.php
@@ -0,0 +1,229 @@
+<?php
+
+$config['key']      = 'YOUR API KEY';
+$config['sendlist'] = [
+    [
+        'address'           =>
+        [
+            'name'  => 'Test',
+            'email' => 'srivprakhar@live.com',
+        ],
+        'substitution_data' =>
+        [
+            'name'                   => 'Live',
+            'saving_electricity_nok' => '12000',
+            'saving_electricity'     => '12',
+            'saving_mobile_nok'      => '12000',
+            'saving_mobile'          => '12',
+            'saving_lan_nok'         => '12000',
+            'saving_lan'             => '12',
+            'saving_total_nok'       => '12000',
+            'saving_total'           => '12',
+        ],
+    ],
+];
+$config['list'] = [
+    "id"          => "test-list-2",
+    "name"        => "graduate_students",
+    "description" => "An email list of graduate students at UMBC",
+    "attributes"  => [
+        "internal_id"   => 112,
+        "list_group_id" => 12321
+    ],
+    "recipients"  => [
+        [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ], [
+            "address"           => [
+                "email" => "wilmaflin@yahoo.com",
+                "name"  => "Wilma"
+            ],
+            "tags"              => [
+                "greeting",
+                "prehistoric",
+                "fred",
+                "flintstone"
+            ],
+            "metadata"          => [
+                "age"   => "24",
+                "place" => "Bedrock"
+            ],
+            "substitution_data" => [
+                "favorite_color" => "SparkPost Orange",
+                "job"            => "Software Engineer"
+            ]
+        ]
+    ],
+];
+
+
+return $config;
--- index.php
+++ index.php
@@ -1,44 +1,68 @@
 <?php
 
 require 'vendor/autoload.php';
+$config = require_once 'config.php';
+
 use SparkPost\SparkPost;
 use GuzzleHttp\Client;
 use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
 
-$key = '43b315128e32ee8662b980740cb99990a12b431c';
-
 $httpClient = new GuzzleAdapter(new Client());
-$sparky = new SparkPost($httpClient, ['key'=>$key]);
+$sparky     = new SparkPost($httpClient, ['key' => $config['key']]);
+$sparky->setOptions(['async' => false]);
 
-$promise = $sparky->transmissions->post([
-    'content' => [
-        //'from' => [
-        //    'name' => 'SparkPost Team',
-        //    'email' => 'from@sparkpostbox.com',
-        //],
-        //'subject' => 'First Mailing From PHP',
-        //'html' => '<html><body><h1>Congratulations, {{name}}!</h1><p>You just sent your very first mailing!</p></body></html>',
-        //'text' => 'Congratulations, {{name}}!! You just sent your very first mailing!',
-        'template_id' => 'test-template'
-    ],
-    'substitution_data' => ['name' => 'Test'],
-    'recipients' => [
-        [
-            'address' => [
-                'name' => 'Test',
-                'email' => 'test@yopmail.com',
-            ],
+
+
+// Send an email with recipients and substitution data
+/*
+$response = $sparky->transmissions->post(
+    [
+        'content'    => [
+            'template_id' => 'test-template'
         ],
-    ],
-]);
+        'recipients' => $config['sendlist']
+    ]
+);
+*/
+
+// Get the content of the list
+// request('GET',"recipient-lists/list-id?show_recipients=true",)
+// $response = $sparky->request('GET', 'recipient-lists/test-list?show_recipients=true');
+
+// Create a recipient list
+// request('POST', 'recipient-lists' ,$payload , $headers);
+// $response = $sparky->request("POST", 'recipient-lists', $config['list']);
+
+// Get all recipient lists
+// request('GET','recipient-lists')
+// $response = $sparky->request('GET','recipient-lists');
+
+
+// Update a recipient list
+// request('PUT','recipient-lists/lis-id',$payload)
+// $response = $sparky->request('PUT','recipient-lists/test-list-2',$config['list']);
+
+// Delete a recipient list
+// request('DELETE','recipient-lists/list-id');
+// $response = $sparky->request('DELETE','recipient-lists/test-list-2');
+
+//$response = $sparky->request('PUT', "recipient-lists/unique_id_4_graduate_students_list", );
+//try {
+////    $response = $sparky->transmissions->post();
+//    echo $response->getStatusCode() . "\n";
+//    print_r($response->getBody()) . "\n";
+//}
+//catch (\Exception $e) {
+//    echo $e->getCode() . "\n";
+//    echo $e->getMessage() . "\n";
+//}
+
 
 try {
-    $response = $promise->wait();
-    echo $response->getStatusCode()."\n";
-    print_r($response->getBody())."\n";
-} catch (\Exception $e) {
-    echo $e->getCode()."\n";
-    echo $e->getMessage()."\n";
+    echo $response->getStatusCode() . "\n";
+    print_r($response->getBody()) . "\n";
 }
-
-echo "I will print out after the promise is fulfilled";
+catch (\Exception $e) {
+    echo $e->getCode() . "\n";
+    echo $e->getMessage() . "\n";
+}
